ebe0d908f6f8d6e00ba59a8c0a3a8f094f4d0848
Katie Wang <34468317+KTWangster@users.noreply.github.com>
2018-05-20 22:01:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyBwJBKuUcJ64mp_MzZUXbM3m71MVymWcME';
+const API_KEY = '';
 
 class App extends Component {
     constructor(props) {
